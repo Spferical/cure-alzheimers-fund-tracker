bc5eeccedc6a67f921b0987a70bf35efc298febb
Matthew Pfeiffer <spferical@gmail.com>
2016-01-13 17:10:37 -0500

    Grab the info we need

--- scrape.py
+++ scrape.py
@@ -13,7 +13,11 @@ if __name__ == '__main__':
     s = search("Cure Alzheimer's Fund", 2015, 2015)
     num = 0
     for x in s:
+        x.fill()
+        stuff = ['title', 'author', 'journal', 'volume', 'issue']
+        for thing in stuff:
+            if thing in x.bib:
+                print("{}: {}".format(thing, x.bib[thing]))
         num += 1
-        print(x)
 
     print("Number of results:", num)
