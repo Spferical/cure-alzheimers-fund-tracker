7280954dfd18aa83d51548a4c78765046b369427
Srinivas Kaza <kazasrinivas3@gmail.com>
2016-01-27 20:07:44 -0500

    REST API implemented

--- scraper/urls.py
+++ scraper/urls.py
@@ -1,7 +1,14 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+from rest_framework import routers
 
 from . import views
 
+router = routers.DefaultRouter()
+router.register(r'authors', views.AuthorViewSet)
+router.register(r'papers', views.PaperViewSet)
+router.register(r'projects', views.ProjectViewSet)
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^api/', include(router.urls))
 ]
--- scraper/views.py
+++ scraper/views.py
@@ -1,8 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import Context, loader
+from rest_framework import viewsets, mixins
+from scraper.serializers import AuthorSerializer, PaperSerializer, ProjectSerializer
+from scraper.models import Author, Paper, Project
 
 
 def index(request):
 	template = loader.get_template("index.html")
 	return HttpResponse(template.render())
+
+
+class AuthorViewSet(viewsets.ModelViewSet):
+	"""
+	API endpoint to access authors
+	"""
+	queryset = Author.objects.all()
+	serializer_class = AuthorSerializer
+
+
+class PaperViewSet(viewsets.ModelViewSet):
+	"""
+	API endpoint to access authors
+	"""
+	queryset = Paper.objects.all()
+	serializer_class = PaperSerializer
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+	"""
+	API endpoint to access authors
+	"""
+	queryset = Project.objects.all()
+	serializer_class = ProjectSerializer
