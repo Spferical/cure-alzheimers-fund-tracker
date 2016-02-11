9fda0bd47b63a703722c0bdb03985de3e2ba7c19
Matthew Pfeiffer <spferical@gmail.com>
2016-02-11 13:06:23 -0500

    Delete (unused) search result length finding

--- scraper/scholar_data.py
+++ scraper/scholar_data.py
@@ -1,7 +1,6 @@
 #!/user/bin/env python3
 
 from __future__ import unicode_literals
-import re
 import requests
 import scholarly
 
@@ -10,29 +9,13 @@ _START_YEAR = '&as_ylo={}'
 _END_YEAR = '&as_yhi={}'
 
 
-class Papers(object):
-    """Wrapper around scholarly._search_scholar_soup that allows one to get the
-    number of papers found in the search with len()"""
-    def __init__(self, query, start_year=None, end_year=None):
-        url = _EXACT_SEARCH.format(requests.utils.quote(query))
-        if start_year:
-            url += _START_YEAR.format(start_year)
-        if end_year:
-            url += _END_YEAR.format(end_year)
-        soup = scholarly._get_soup(url)
-        results = soup.find('div', id='gs_ab_md').text
-        self.num = int(re.search(r'\d+ results', results).group(0).split()[0])
-
-        self.papers = scholarly._search_scholar_soup(soup)
-
-    def __len__(self):
-        return self.num
-
-    def __iter__(self):
-        return self.papers
-
-
 def get_published_papers(start_year=None, end_year=None):
     """ Returns a generator that returns dicts with paper metadata."""
-    return Papers("Cure Alzheimer's Fund",
-                  start_year=start_year, end_year=end_year)
+    query = "Cure Alzheimer's Fund"
+    url = _EXACT_SEARCH.format(requests.utils.quote(query))
+    if start_year:
+        url += _START_YEAR.format(start_year)
+    if end_year:
+        url += _END_YEAR.format(end_year)
+    soup = scholarly._get_soup(url)
+    return scholarly._search_scholar_soup(soup)
